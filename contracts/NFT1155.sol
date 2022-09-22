6ae84a61efb537530f0f2dba79ee13e9a9e9ab2c
yashvardhan <yashvardhan@antiersolutions.com>
2022-09-22 16:44:23 +0530

    1155 Updates

--- contracts/NFT1155.sol
+++ contracts/NFT1155.sol
@@ -1,4 +1,116 @@
 
+// File: @openzeppelin/contracts/utils/Context.sol
+
+
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// File: @openzeppelin/contracts/access/Ownable.sol
+
+
+// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
 // File: @openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol
 
 
@@ -1354,9 +1466,14 @@ abstract contract ERC1155URIStorageUpgradeable is Initializable, ERC1155Upgradea
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-contract token1155 is ERC1155URIStorageUpgradeable{
 
-    mapping(uint256 => bool)private minted;
+contract token1155 is ERC1155URIStorageUpgradeable,Ownable{
+    struct tokenAmount{
+        uint256 mintedAmount;
+        uint256 maxAmount;
+    }
+    
+    mapping(uint256 => tokenAmount)private token;
 
     function initialize(string memory _URI) external virtual initializer{
 
@@ -1364,19 +1481,31 @@ contract token1155 is ERC1155URIStorageUpgradeable{
     }
 
     function mint(address _to, uint256 _tokenID, uint256 _amount, bytes memory _data) external virtual {
-        require(!minted[_tokenID],"Token already minted");
+        require(token[_tokenID].maxAmount > 0,"Token ID not availale");
+        token[_tokenID].mintedAmount+= _amount;
+        require(token[_tokenID].mintedAmount<=token[_tokenID].maxAmount,"Token max amount reached");
         _mint(_to, _tokenID, _amount, _data);
-        minted[_tokenID] = true;
+    
     }
 
-    function setURI(uint256 _tokenID,string memory _URI) external virtual {
+    function setURI(uint256 _tokenID,string memory _URI) external virtual onlyOwner {
+        require(token[_tokenID].mintedAmount > 0,"Invalid token ID");
+        require(bytes(_URI).length != 0,"URI can't be empty ");
         _setURI(_tokenID, _URI);
     }
 
-    function isMinted(uint256 _tokenID) external virtual view returns(bool) {
-        if(minted[_tokenID])
-            return true;
-        else 
-            return false;    
+    function setAmount(uint256 _tokenID,uint256 _amount) external virtual onlyOwner{
+       require(_amount !=0,"Amount can't be zero");
+       tokenAmount storage info = token[_tokenID];
+       info.maxAmount = _amount;
+    }
+
+
+    function _msgSender() internal view virtual override(ContextUpgradeable,Context) returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual override(ContextUpgradeable,Context) returns (bytes calldata) {
+        return msg.data;
     }
 }
